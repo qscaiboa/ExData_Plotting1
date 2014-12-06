698da8255b047761a7d9bb7cd728aca8e273d194
caipine <caipine@users.noreply.github.com>
2014-12-05 23:53:50 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-w1 <- read.table("https://github.com/caipine/ExData_Plotting1/blob/master/household_power_consumption.txt", sep = ";")
+w1 <- read.csv("household_power_consumption.txt", sep = ";")
 w1 <- w1[w1$Date %in% as.factor(c("1/2/2007","2/2/2007")),]
 png(filename = "plot1.png", width = 480, height = 480, units = "px", pointsize = 12,  bg = "white",)
 with(w1, hist(as.numeric(Global_active_power), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red"))
